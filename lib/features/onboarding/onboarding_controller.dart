6d80257a030296a907f5f2c16bbbf541b433fb90
fivontwov <100524815+21020764-VuPhuongHong@users.noreply.github.com>
2024-03-31 15:44:32 +0700

    add splash screen

---
+++ lib/features/onboarding/onboarding_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  void updatePageIndicator(index) {}
+}
+
---
+++ lib/utils/consts/image_paths.dart
@@ -0,0 +1,5 @@
+class Images {
+  static const String onboarding1 = "assets/images/onboarding/onboard1.json";
+  static const String onboarding2 = "assets/images/onboarding/onboard2.json";
+  static const String onboarding3 = "assets/images/onboarding/onboard3.json";
+}
